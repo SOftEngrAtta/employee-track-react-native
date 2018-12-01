cff755c5155c170bd21dc9432a2669d01f81ea91
atta ur rehman <attarehman@axact.com>
2018-12-01 18:05:25 +0500

    working on side bar

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
-import { View, Text, Dimensions} from 'react-native';
+import { View,  Dimensions , Image , Text ,  StatusBar , ProgressBarAndroid} from 'react-native';
+import * as Progress from 'react-native-progress';
 
+
+//image
+import profileImage from '../../assets/profileImage.jpg'
 
 
 let ScreenHeight = Dimensions.get("window").height;
@@ -33,7 +37,16 @@ export class SideBarTab extends Component {
                     (this.state.showMenu) ?
 
                         <View style={ sideMenuCls.container } >
-                            <Text>Home</Text>
+                            <View style={ sideMenuCls.profileImagePrnt }>
+                                <Image source={ profileImage } style={ sideMenuCls.profileImage }/>
+                                <Text style={ sideMenuCls.profileImageText }> Atta Ur Rehman (20%)</Text>
+                                
+                            </View>
+
+
+                                <Progress.Bar progress={0.3} animated={ true } color="#fff" height={6} width={70} borderColor="#fff"  style={{ marginLeft: 70, marginTop: 5}}/>
+                          
+
                         </View>
 
                         : null
@@ -50,9 +63,27 @@ const sideMenuCls = {
         width: '50%',
         height: ScreenHeight,
         top: 0,
-        backgroundColor: '#222',
+        backgroundColor: '#2e416b',
         zIndex: 2,
         transition: '2s'
 
+    },
+    profileImagePrnt:{
+        false: 1,
+        flexDirection: 'row', 
+        top: 20,
+        left: 20,
+    },
+    profileImage : {
+        width: 40, height: 40,
+        borderRadius : 50,
+        borderColor: '#fff',
+        borderWidth: 2
+    },
+    profileImageText : {
+        color: "#fff",
+        marginTop: 5,
+        fontSize: 10,
+        marginLeft: 10 
     }
 }
--- src/router/router.js
+++ src/router/router.js
@@ -22,9 +22,9 @@ export class Routers extends Component {
         return (
             <Router >
                 <Stack key="root" hideNavBar>
-                    <Scene key="login" component={Login}  title="login" initial />
+                    <Scene key="login" component={Login}  title="login"  />
                     <Scene key="signup" component={SignUp}  title="signup" />
-                    <Scene key="dashboard" component={Dashboard} title="dashboard" />
+                    <Scene key="dashboard" component={Dashboard} title="dashboard" initial/>
                 </Stack>
             </Router>
         )
