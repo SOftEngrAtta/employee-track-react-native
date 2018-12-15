a72361c46e5397e0c0ca716d9b4ea1166f82f7fd
atta ur rehman <attarehman@axact.com>
2018-12-15 12:22:58 +0500

    nitifucation implemented

--- android/app/src/main/java/com/myapp/MainApplication.java
+++ android/app/src/main/java/com/myapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.myapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.imagepicker.ImagePickerPackage;
@@ -32,6 +33,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativePushNotificationPackage(),
             new SplashScreenReactPackage(),
             new ImagePickerPackage(),
             new RNGestureHandlerPackage(),
--- src/components/view-status/status.js
+++ src/components/view-status/status.js
@@ -1,16 +1,55 @@
-import React , { Component } from 'react';
-import { View , Text } from 'native-base';
+import React, { Component } from 'react';
+import { View, Text, Picker, AppState } from 'react-native'
 
+import PushNotification from 'react-native-push-notification';
 
-// components 
 import { HeaderComponent } from '../header/header';
+// import { PushController } from './pushController'
 
-export class StatusComponent extends Component{
-    render(){
+export class StatusComponent extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { seconds: 5 }
+    }
+
+    componentDidMount() {
+        
+        AppState.addEventListener('change', this.handlerStateChange);
+    }
+
+    componentWillMount() {
+        AppState.removeEventListener('change', this.handlerStateChange);
+    }
+
+    handlerStateChange = (appState) => {
+        if (appState == 'background') {
+            console.log('app is in background ', this.state.seconds);
+            let date = new Date(Date.now() + (this.state.seconds * 1000));
+
+            PushNotification.localNotificationSchedule({
+                message: "My Notification Message",
+                date,
+            });
+        }
+    }
+
+
+    render() {
         return (
-            <View>
-                <HeaderComponent title="Status" getHistory={this.props} bgColor="#e74003"/>
-                <Text> Hello </Text>
+            <View >
+                <HeaderComponent title="Status" getHistory={this.props} bgColor="#e74003" />
+                <View >
+                    <Text> Hello </Text>
+                    <Picker style={{ width: 100 }}
+                        selectedValue={this.state.seconds}
+                        onValueChange={(seconds) => this.setState({ seconds })}>
+                        <Picker.Item label="5" value={5} />
+                        <Picker.Item label="10" value={10} />
+                        <Picker.Item label="15" value={15} />
+                    </Picker>
+                    {/* <PushController /> */}
+                </View>
             </View>
         )
     }
